29626caf0de12b1821a3bcd9daeaeeeba3c0c760
Russell Sim <russell.sim@monash.edu>
2010-11-18 21:18:17 +1100

    fixed md5 deprecation warning and reorded imports

--- txirods/client.py
+++ txirods/client.py
@@ -18,14 +18,14 @@
 #
 #############################################################################
 
+import logging
+from hashlib import md5
+from construct import Container
 from twisted.internet import reactor, defer
 from twisted.internet.protocol import ClientFactory
 from twisted.python import log, failure
 from txirods.encoding import binary as messages
 from txirods import api
-from md5 import md5
-from construct import Container
-import logging
 
 from protocol import IRODSChannel, Request
 
