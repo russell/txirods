8cbfac97fde3ab5c38806e764a4afa7a7384ce00
Russell Sim <russell.sim@gmail.com>
2011-02-02 20:01:50 +1100

    added initial cli interaction test

---
+++ txirods/test/test_interaction.py
@@ -0,0 +1,43 @@
+#############################################################################
+#
+# Copyright (c) 2011 Russell Sim <russell.sim@gmail.com> and Contributors.
+# All Rights Reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+import os
+#import unittest
+from twisted.trial import unittest
+from scripttest import TestFileEnvironment
+base_path = os.path.join(os.getcwd(), 'interactive_tests')
+env = TestFileEnvironment(base_path, cwd=os.getcwd())
+
+
+class InteractionTestCase(unittest.TestCase):
+    if not 'TXIRODS_TEST' in os.environ:
+        skip = "interactive tests disabled, to enable export TXIRODS_TEST"
+
+    def test_simple_collection(self):
+        filename = 'testdir'
+        env.run('./bin/imkdir %s' % filename)
+        env.run('./bin/ils %s' % filename)
+        env.run('./bin/irmdir %s' % filename)
+
+#import pdb
+#pdb.set_trace()
+
+if __name__ == "__main__":
+    unittest.runtests()
